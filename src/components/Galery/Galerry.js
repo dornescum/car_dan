715c846be7c452f44732aaf2ab890e483322566c
Daniel <cristea.daniel.petrut@gmail.com>
2021-10-02 17:29:57 +0300

    Update main image and small fixes

--- src/components/Galery/Galerry.js
+++ src/components/Galery/Galerry.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {SRLWrapper} from "simple-react-lightbox";
 import './gallery.css'
-import img_1 from '../../assets/webp/1.webp';
-import img_1sm from '../../assets/webp/1-sm.webp';
+// import img_1 from '../../assets/webp/1.webp';
+// import img_1sm from '../../assets/webp/1-sm.webp';
 import img_2 from '../../assets/webp/2.webp';
 import img_2sm from '../../assets/webp/2-sm.webp';
 import img_3 from '../../assets/webp/3.webp';
@@ -14,9 +14,9 @@ import img_5 from '../../assets/webp/5.webp';
 import img_5sm from '../../assets/webp/5-sm.webp';
 import img_6 from '../../assets/webp/6.webp';
 import img_6sm from '../../assets/webp/6-sm.webp';
-import img_7 from '../../assets/webp/7.webp';
+// import img_7 from '../../assets/webp/7.webp';
 import img_7s from '../../assets/webp/7.1.webp';
-import img_7sm from '../../assets/webp/7-sm.webp';
+// import img_7sm from '../../assets/webp/7-sm.webp';
 import img_8 from '../../assets/webp/8.webp';
 import img_8sm from '../../assets/webp/8-sm.webp';
 import img_9 from '../../assets/webp/9.webp';
@@ -27,8 +27,7 @@ import img_11 from '../../assets/webp/11.webp';
 import img_11sm from '../../assets/webp/11-sm.webp';
 import img_12 from '../../assets/webp/12.webp';
 import img_12sm from '../../assets/webp/12-sm.webp';
-import img_test from '../../assets/17.webp';
-import test from '../../assets/1.jpg';
+import mainImage from '../../assets/webp/main.webp';
 
 
 const Galerry = () => {
@@ -42,9 +41,9 @@ const Galerry = () => {
 				<div className="row">
 
 					<div className="col-lg-3 col-sm-6 py-2">
-						<a href={test} className="photo">
+						<a href={mainImage} className="photo">
 							<img
-								src={test}
+								src={mainImage}
 								alt="Picture of the author"
 								srl_gallery_image="true"
 							/>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import img from '../../assets/webp/1.webp';
 import './hero.css'
-import test from '../../assets/1.jpg';
-
-
-
-
+import mainImage from '../../assets/webp/main.webp';
 
 const Hero = () => {
 	return (
 		<div className="hero">
-			<img src={test} alt="Travel" className="hero__image" id='home' />
+			<img src={mainImage} alt="Opel Astra G" className="hero__image" id='home' />
 
-			<h1 className="hero__title">Best car ever.</h1>
+			<h1 className="hero__title">Masina unui nou inceput</h1>
 		</div>
 	);
 };
